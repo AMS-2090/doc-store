d6d0feda3184c74b42850f6e0defa03a92dc9264
Arkadiusz <ben4815@gmail.com>
2017-06-30 23:58:43 +0200

    Add Spring Boot base and one controller example

---
+++ src/main/java/ams/docstore/DocStoreApplication.java
@@ -0,0 +1,12 @@
+package ams.docstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocStoreApplication.class, args);
+	}
+}
