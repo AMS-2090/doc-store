766e2163ea41a309cfb6c0ee2679ed738c190687
Arkadiusz <ben4815@gmail.com>
2017-07-04 22:41:19 +0200

    Add Service interface and implementation
    - DocumentService interface added,
    - DocumentServiceImpl added as interface implementation.

---
+++ src/main/java/ams/docstore/service/DocumentService.java
@@ -0,0 +1,10 @@
+package ams.docstore.service;
+
+import java.util.List;
+
+import ams.docstore.domain.Document;
+
+public interface DocumentService {
+
+	List<Document> getAllDocuments();
+}
---
+++ src/main/java/ams/docstore/service/impl/DocumentServiceImpl.java
@@ -0,0 +1,23 @@
+package ams.docstore.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ams.docstore.domain.Document;
+import ams.docstore.domain.repository.DocumentRepository;
+import ams.docstore.service.DocumentService;
+
+@Service
+public class DocumentServiceImpl implements DocumentService {
+
+	@Autowired
+	private DocumentRepository documentRepository;
+	
+	@Override
+	public List<Document> getAllDocuments() {
+		return documentRepository.getAllDocuments();
+	}
+
+}
