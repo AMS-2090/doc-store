f6c41fd907bca444ca0273549c60e2f4f93e126a
Arkadiusz <ben4815@gmail.com>
2017-07-04 22:44:28 +0200

    Add first Document Controller
    Controller has a method which lists all documents from repository. It handles '/all' request.

---
+++ src/main/java/ams/docstore/controller/DocumentController.java
@@ -0,0 +1,22 @@
+package ams.docstore.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ams.docstore.domain.Document;
+import ams.docstore.service.DocumentService;
+
+@RestController
+public class DocumentController {
+
+	@Autowired
+	private DocumentService documentService;
+	
+	@RequestMapping("/all")
+	public List<Document> allDocuments() {
+		return documentService.getAllDocuments();
+	}
+}
