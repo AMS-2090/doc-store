648ed2826347bd23bc4846ff29d5c24e3d7eeeb1
Arkadiusz <ben4815@gmail.com>
2017-07-04 22:39:11 +0200

    Add Repository interface and implementation
    - DocumentRepository interface added,
    - InMemoryDocumentRepository added as temporary implementation.

---
+++ src/main/java/ams/docstore/domain/repository/DocumentRepository.java
@@ -0,0 +1,10 @@
+package ams.docstore.domain.repository;
+
+import java.util.List;
+
+import ams.docstore.domain.Document;
+
+public interface DocumentRepository {
+
+	List<Document> getAllDocuments();
+}
---
+++ src/main/java/ams/docstore/domain/repository/impl/InMemoryDocumentRepository.java
@@ -0,0 +1,39 @@
+package ams.docstore.domain.repository.impl;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import ams.docstore.domain.Document;
+import ams.docstore.domain.repository.DocumentRepository;
+
+/*
+ * Temporary hard-coded docs repo for testing purposes
+ */
+@Repository
+public class InMemoryDocumentRepository implements DocumentRepository {
+
+	private List<Document> listOfDocuments;
+	
+
+	public InMemoryDocumentRepository() {
+		
+		listOfDocuments = new ArrayList<Document>();
+		
+		Document firstDoc = new Document("001", "first", "PDF");
+		firstDoc.setSource("Scanner_1");
+		firstDoc.setDate(LocalDate.now());
+		firstDoc.setTime(LocalTime.now());
+		firstDoc.setDescription("This is the first document in this temporary repository.");
+		
+		listOfDocuments.add(firstDoc);
+	}
+	
+	@Override
+	public List<Document> getAllDocuments() {
+		return listOfDocuments;
+	}
+}
