085079604f57100b1b81099742ada87c3c187efc
Arkadiusz <ben4815@gmail.com>
2017-07-13 21:15:00 +0200

    Add controller method to handle Document adding
    - controller method handles POST request, maps request's body to Document object and adds it to Repo using document service

--- src/main/java/ams/docstore/controller/DocumentController.java
+++ src/main/java/ams/docstore/controller/DocumentController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import ams.docstore.domain.Document;
@@ -24,5 +26,10 @@ public class DocumentController {
 	@RequestMapping("/documents/{id}")
 	public Document getDocumentById(@PathVariable String id) {
 		return documentService.getDocumentById(id);
+	}
+	
+	@RequestMapping(value = "/documents", method = RequestMethod.POST)
+	public void addDocument(@RequestBody Document document) {
+		documentService.addDocument(document);
 	}
 }
